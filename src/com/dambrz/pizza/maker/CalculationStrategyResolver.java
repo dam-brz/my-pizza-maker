1f3acc76b1fd2e0891690eb37d7e20898a5eded1
dam-brz <dam-brz@wp.pl>
2021-08-18 20:05:57 +0200

    Create CalculationStrategyResolver class

---
+++ src/com/dambrz/pizza/maker/CalculationStrategyResolver.java
@@ -0,0 +1,19 @@
+package com.dambrz.pizza.maker;
+
+class CalculationStrategyResolver {
+
+    CalculationStrategy chooseStrategy(Size size) {
+        switch (size) {
+            case SMALL:
+                return new SmallPizzaCalculationStrategy();
+            case MEDIUM:
+                return new MediumPizzaCalculationStrategy();
+            case LARGE:
+                return new LargePizzaCalculationStrategy();
+            case EXTRA_LARGE:
+                return new ExtraLargePizzaCalculationStrategy();
+            default:
+                throw new IllegalStateException("Unknown size=" + size);
+        }
+    }
+}
