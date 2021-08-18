b71ab794a0990ae91ff8deaa508c6bf38443112b
dam-brz <dam-brz@wp.pl>
2021-08-18 19:25:10 +0200

    Modify start() method in PizzaMaker class

--- src/com/dambrz/pizza/maker/PizzaMaker.java
+++ src/com/dambrz/pizza/maker/PizzaMaker.java
@@ -1,10 +1,6 @@
 package com.dambrz.pizza.maker;
 
-import java.util.List;
 import java.util.Scanner;
-
-import static com.dambrz.pizza.maker.Dough.THICK_CRUST;
-import static com.dambrz.pizza.maker.Topping.PINEAPPLE;
 
 public class PizzaMaker {
     public static void main(String[] args) {
@@ -16,7 +12,8 @@ public class PizzaMaker {
 
         var pizza = createPizza();
 
-        calculatePrice(pizza);
+        PizzaPriceCalculator pizzaPriceCalculator = new PizzaPriceCalculator(pizza);
+        pizzaPriceCalculator.calculatePizzaPrice();
     }
 
     private Pizza createPizza() {
@@ -57,100 +54,5 @@ public class PizzaMaker {
         }
     }
 
-    private static double getMeatToppingsPercentage(List<Topping> toppings) {
-        long count = toppings.stream()
-                .filter(Topping::isMeat)
-                .count();
 
-        return ((double) count/(double) toppings.size()) * 100;
-    }
-
-    private int calculatePrice(Pizza pizza) {
-        Size size = pizza.getSize();
-        int price = 0;
-
-        switch (size) {
-            case SMALL:
-                price = calculatePriceForSmallPizza(pizza);
-                break;
-            case MEDIUM:
-                price = calculatePriceForMediumPizza(pizza);
-                break;
-            case LARGE:
-                price = calculatePriceForLargePizza(pizza);
-                break;
-            case EXTRA_LARGE:
-                price = calculatePriceForExtraLargePizza(pizza);
-                break;
-            default:
-                throw new IllegalStateException("Unknown size=" + size);
-        }
-
-        System.out.println("Price: " + price);
-        return price;
-    }
-
-    private int calculatePriceForSmallPizza(Pizza pizza) {
-        int price;
-        if (pizza.getToppings().size() <= 2) {
-            price = 18;
-        } else {
-            price = 22;
-        }
-        return price;
-    }
-
-    private int calculatePriceForMediumPizza(Pizza pizza) {
-        int price;
-        if (pizza.getToppings().size() <= 3) {
-            if (getMeatToppingsPercentage(pizza.getToppings()) < 50) {
-                price = 25;
-            } else {
-                price = 27;
-            }
-        } else {
-            if (getMeatToppingsPercentage(pizza.getToppings()) < 40) {
-                price = 27;
-            } else {
-                price = 29;
-            }
-        }
-        return price;
-    }
-
-    private int calculatePriceForLargePizza(Pizza pizza) {
-        int price;
-        if (pizza.getToppings().size() <= 3) {
-            if (getMeatToppingsPercentage(pizza.getToppings()) < 40) {
-                price = 30;
-            } else {
-                price = 35;
-            }
-        } else {
-            if (pizza.getToppings().contains(PINEAPPLE)) {
-                price = 37;
-            } else {
-                price = 40;
-            }
-        }
-
-        if (pizza.getDough() == THICK_CRUST) {
-            price += 5;
-        }
-        return price;
-    }
-
-    private int calculatePriceForExtraLargePizza(Pizza pizza) {
-        int price;
-        if (pizza.getToppings().size() <= 2) {
-            price = 40;
-        } else {
-            price = 50;
-        }
-
-        if (pizza.getDough() == THICK_CRUST) {
-            price +=10;
-        }
-        return price;
-    }
 }
